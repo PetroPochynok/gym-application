ca57ba998abb17ed253d5b9d78a2323b4b6004a9
Petro Pochynok <p.pochynok123@gmail.com>
2026-06-15 14:26:20 +0300

    register gym-crm and gym-workload as eureka clients

--- gym-crm/src/main/java/com/epam/gym/crm/GymCrmApplication.java
+++ gym-crm/src/main/java/com/epam/gym/crm/GymCrmApplication.java
@@ -2,10 +2,12 @@ package com.epam.gym.crm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 import java.util.Locale;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class GymCrmApplication {
 
     public static void main(String[] args) {
--- gym-workload/src/main/java/com/epam/gym/workload/GymWorkloadApplication.java
+++ gym-workload/src/main/java/com/epam/gym/workload/GymWorkloadApplication.java
@@ -2,8 +2,10 @@ package com.epam.gym.workload;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class GymWorkloadApplication {
 
 	public static void main(String[] args) {
